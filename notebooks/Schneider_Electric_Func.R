43d7cfa08bd573d4d11b81efdaca665ca868571e
Aum Pandya <aumpandya.95@gmail.com>
2021-05-24 19:38:28 +0530

    Initial commit

---
+++ notebooks/Schneider_Electric_Func.R
@@ -0,0 +1,10 @@
+##############################################################
+## Functions for analyzing Schneider Electric Data Set
+##
+## Date: 20-May-2021
+##############################################################
+
+avg_per_time = function(x) {
+    return(mean(x, na.rm=TRUE))
+}
+
