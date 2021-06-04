a622f2dc78ddf66a01e38791b47972416a3b7f06
Aum Pandya <aumpandya.95@gmail.com>
2021-06-04 21:40:40 +0530

    FEAT: converted src/data to a package

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -7,3 +7,6 @@ The final data is then stored in the main data folder's processed directory.
 
 The processed data sets are then used in the jupyter notebooks present in the notebooks directory.
 """
+__all__ = ['extract_data']
+
+from src.data.extraction import extract_data
