a0bb8e2a0dffb8225e698714eed3bfc160850a77
Aum Pandya <aumpandya.95@gmail.com>
2021-06-06 04:19:26 +0530

    Update: Updated command instructions to explain the process of data extraction and pre-processing, if any

--- process_data.sh
+++ process_data.sh
@@ -6,8 +6,12 @@ through to generate processed data in the /data/processed/ folder.\n------------
 printf "STEP 1:\n"
 for f in ./data/raw/*.zip;
 do
-  printf "\tFound the zipped data folder -> %s \n\t Extracting the files..." "$f"
-  unzip -j -n "$f" -d ./data/raw/
+  printf "\tFound the zipped data folder -> %s" "$f"
+  printf "\n\tExtracting the files..."
+  printf "\nData files used are,\n"
+
+  unzip -j -n "$f" -d ./data/raw/  # Files will be unzipped, however already existing files will not be replaced
+
   for l in $(unzip -Z1 "$f" | tail -5);
     do
       if basename "$l" | grep -i -s '\btraining\b'
@@ -23,11 +27,12 @@ do
           printf "\t" | sed '/^[[:space:]]*$/d'
       fi
     done
-printf "\n----------------------------------------------\n"
 done;
 
-echo "This is the file -> $TRAIN"
-echo "This is the file -> $METADATA"
+printf ""
+printf "\n----------------------------------------------\nSTEP 2:\n\tStored the file names :D\n"
+printf "\tHere is the main file name -> %s" "$TRAIN"
+printf "\n\tHere is the file name for the building data -> %s" "$METADATA"
 
 # Make executable with chmod +x process_data.sh
 python ./src/data/extraction.py "$TRAIN" "$METADATA"
