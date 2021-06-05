e07f357f284a8efb079fafce28a816f2832dd929
Aum Pandya <aumpandya.95@gmail.com>
2021-06-06 04:29:28 +0530

    Update: Completed data pre-processing bash script

--- process_data.sh
+++ process_data.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-printf "Starting the data processing operation. \nData from the /data/raw folder's zip file will be unzipped and processed
-through to generate processed data in the /data/processed/ folder.\n----------------------------------------------\n"
+printf "Starting the data processing operation. \nData will be extracted from the /data/raw directory's zip file and processed
+through to generate processed data in the /data/processed/ directory.\n----------------------------------------------\n"
 
 printf "STEP 1:\n"
 for f in ./data/raw/*.zip;
 do
   printf "\tFound the zipped data folder -> %s" "$f"
-  printf "\n\tExtracting the files..."
-  printf "\nData files used are,\n"
+  printf "\n\tExtracting the files...\n\t"
 
   unzip -j -n "$f" -d ./data/raw/  # Files will be unzipped, however already existing files will not be replaced
+
+  printf "\nData files used are,\n"
 
   for l in $(unzip -Z1 "$f" | tail -5);
     do
@@ -35,4 +36,8 @@ printf "\tHere is the main file name -> %s" "$TRAIN"
 printf "\n\tHere is the file name for the building data -> %s" "$METADATA"
 
 # Make executable with chmod +x process_data.sh
+printf "\n----------------------------------------------\nSTEP3:\n\tExecuting Python script to generate processed data..."
+printf "\n\tProcessed data will be saved in /data/processed/ folder\n"
+
 python ./src/data/extraction.py "$TRAIN" "$METADATA"
+printf "\nProcess has been completed!\n----------------------------------------------\n"
